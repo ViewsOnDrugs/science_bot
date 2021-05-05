88bdb9ab397801332e88ee836a24a3d4a74e5a7a
farcilal <farcilas@gmail.com>
2021-05-04 20:42:02 -0500

    update setup

--- setup.py
+++ setup.py
@@ -14,17 +14,25 @@ setup(
         "sci-com",
         "drug policy",
         "research",
-	"science",
+        "science",
     ],
     entry_points={
         "console_scripts": [
             "scibot=scibot.what_a_c:main",
         ]
     },
-    install_requires=["tweepy",
-                    "feedparser",
-                    "schedule",
-                    "python-dotenv",],
+    install_requires=[
+        "beautifulsoup4==4.9.3",
+        "feedparser==6.0.2",
+        "oauthlib==3.1.0",
+        "python-dotenv==0.15.0",
+        "requests==2.25.1",
+        "requests-oauthlib==1.3.0",
+        "schedule==0.6.0",
+        "telebot==0.0.4",
+        "Telethon==1.18.2",
+        "tweepy==3.10.0",
+        "urllib3==1.26.2",
+    ],
     zip_safe=False,
-
 )
